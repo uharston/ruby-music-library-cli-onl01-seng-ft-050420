330058b9412ac9e90867c37ea414539c42ed614a
Uriah Harston <uharston@gmail.com>
2020-05-23 01:59:34 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -14,6 +14,7 @@ class MusicLibraryController
    puts "To play a song, enter 'play song'."
    puts "To quit, type 'exit'."
    puts "What would you like to do?"
+   response = gets.strip
  end
 
 end
