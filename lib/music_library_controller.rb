2862b66ba2cd1985438ba165e534a3eea21da5c8
Uriah Harston <uharston@gmail.com>
2020-05-23 02:19:42 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -17,17 +17,17 @@ class MusicLibraryController
    input = gets.strip
 
    case input
-    when "list songs"
+   when 'list songs'
       list_songs
-    when "list artists"
+    when 'list artists'
       list_artists
-    when "list genres"
+    when 'list genres'
       list_genres
-    when "list artist"
+    when 'list artist'
       list_songs_by_artist
-    when "list genre"
+    when 'list genre'
       list_songs_by_genre
-    when "play song"
+    when 'play song'
       play_song
     end
  end
