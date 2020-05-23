0775055df56c738ecdaeb21d72da8586a16e2531
Uriah Harston <uharston@gmail.com>
2020-05-23 02:21:19 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -5,6 +5,8 @@ class MusicLibraryController
  end
 
  def call
+   input = ''
+   while input != 'exit'
    puts "Welcome to your music library!"
    puts "To list all of your songs, enter 'list songs'."
    puts "To list all of the artists in your library, enter 'list artists'."
