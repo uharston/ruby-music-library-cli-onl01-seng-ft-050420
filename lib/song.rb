488f8949dbc99e0cd87728f84e227f7a607d4491
Uriah Harston <uharston@gmail.com>
2020-05-23 03:17:17 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -8,19 +8,6 @@ class Song
     @name = name
     self.artist = artist if artist
     self.genre = genre if genre
-    # if artist != nil
-    #   self.artist = artist
-    # end
-  end
-
-  def genre=(genre)
-    @genre = genre
-    genre.add_song(self)
-  end
-
-  def artist=(artist)
-     @artist = artist
-    artist.add_song(self)
   end
 
   def self.find_by_name(input)
@@ -31,10 +18,6 @@ class Song
     found = self.find_by_name(input)
       found ? found : self.create(input)
     end
-
-#     def self.find_or_create_by_name(input)
-#   input = self.find_by_name(input) ? song : Song.new(input)
-# end
 
   def self.all
     @@all
@@ -53,4 +36,89 @@ class Song
     @@all[-1]
   end
 
+    def genre=(genre)
+      @genre = genre
+      genre.add_song(self)
+    end
+
+    def artist=(artist)
+       @artist = artist
+      artist.add_song(self)
+    end
+    def self.new_from_filename(filename)
+      artist, song, genre = filename.gsub(".mp3", "").split(" - ")
+      artist_instance = Artist.find_or_create_by_name(artist)
+      genre_instance = Genre.find_or_create_by_name(genre)
+      new_song = self.new(song, artist_instance, genre_instance)
+  end
+  def self.create_from_filename(filename)
+    @@all << self.new_from_filename(filename)
+    @@all
+  end
+
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  # def self.new_from_filename(filename)
+  #   artist, song, genre = filename.gsub(".mp3", "").split(" - ")
+  #   new_song = self.new(song)
+  #   new_song.artist = artist
+  #   new_song.genre = genre
+  #
+  # end
+
+
+
+# new_song.artist= new_artist
+# new_song.genre= new_genre
+# binding.pry
+
+#new_song = self.find_or_create_by_name(song)
+# new_song.artist= artist
+# new_song.genre= genre
+# @artist = result[0]
+# @song = result[1]
+# @genre = result[2]
+
+#self.find_or_create_by_name(result[0], result[1],result[2].gsub(".mp3", ""))
+
+
+#   parts = filename.split(" - ")
+#   artist_name = parts[0]
+#   song_name = parts[1].gsub(".mp3", "")
+#
+#   song = self.new
+#   song.name = song_name
+#   song.artist_name = artist_name
+#   song
