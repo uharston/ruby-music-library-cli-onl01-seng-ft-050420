dc0b5deb6c790d50e1a06ed4c68061137a786b58
Uriah Harston <uharston@gmail.com>
2020-05-23 02:42:28 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -40,7 +40,7 @@ class MusicLibraryController
  def list_songs
 Song.all.sort{|a, b| a.name <=> b.name }.each_with_index do |e, i|
    puts "#{i+1}. #{s.artist.name} - #{s.name} - #{s.genre.name}"
- end 
+ end
 
  end
 
